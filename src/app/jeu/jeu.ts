a2498e78ddd6f41950f7343bd1a64f76a710ff49
loick_mercier <loick_mercier@ens.univ-artois.fr>
2021-04-01 12:44:28 +0200

    Fix général

--- src/app/jeu/jeu.ts
+++ src/app/jeu/jeu.ts
@@ -2,16 +2,18 @@ import {Commentaire} from './commentaire';
 import {Statistique} from './statistique';
 import {Tarif} from './tarif';
 import {Mecanic} from '../_models/mecanic';
+import {Editeur} from './editeur';
+import {Theme} from './theme';
 
 export interface Jeu {
   id: number;
   nom: string;
   description: string;
-  editeur_id: number;
+  editeur_id: Editeur;
   regles: string;
   langue: string;
   url_media: string;
-  theme_id: number;
+  theme_id: Theme;
   nombre_joueurs: string;
   duree: string;
   commentaires: Commentaire[];
